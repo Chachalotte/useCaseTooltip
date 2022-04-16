e5c6d2b669c88c233abcdb65609d2d4291c439d0
Charlotte Kindt <37578645+Chachalotte@users.noreply.github.com>
2022-04-16 15:54:22 +0200

    added shared module, made the home component and the 4 cases for the tooltip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UseCaseComponent} from './pages/use-case/use-case.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  
+    path: '',
+    redirectTo: 'use-case',
+    pathMatch: 'full' 
+  },
+  {
+    path: 'use-case',
+    component: UseCaseComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
